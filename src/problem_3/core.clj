e882dea370d135f5c238744602efa13a66383def
Ryan Mulcahy <ryan@primeacademy.io>
2016-12-07 00:24:28 -0600

    Problem 3

---
+++ src/problem_3/core.clj
@@ -0,0 +1,36 @@
+(ns problem-3.core
+  (:require [clojure.string :refer [split split-lines trim]]))
+
+(defn parse-input
+  [input]
+  (->> input
+       split-lines
+       (map trim)
+       (map #(split % #"\s+"))
+       (map (fn [strings] (map #(Integer/valueOf %) strings)))))
+
+(defn triangle?
+  [a b c]
+  (and (> (+ a b) c)
+       (> (+ a c) b)
+       (> (+ b c) a)))
+
+(def reorder-triple (juxt
+                     (partial map first)
+                     (partial map second)
+                     (partial map #(nth % 2))))
+
+;; way nicer solution from the interwebz
+(defn transpose [matrix]
+  (apply map list matrix))
+
+(defn reorder-list [list]
+  (->> list
+       (partition 3)
+       (mapcat transpose)))
+
+(->> (slurp "input/problem3.txt")
+     parse-input
+     reorder-list
+     (filter (partial apply triangle?))
+     count)
---
+++ test/problem_3/core_test.clj
@@ -0,0 +1,37 @@
+(ns problem-3.core-test
+  (:require [problem-3.core :refer [triangle? reorder-triple reorder-list transpose]]
+            [clojure.test :refer [deftest testing is]]))
+
+(deftest triangle?-test
+  (testing "the triangle predicate"
+    (is (false? (triangle? 5 10 25)))
+    (is (false? (triangle? 5 25 10)))
+    (is (false? (triangle? 10 5 25)))
+    (is (false? (triangle? 10 25 5)))
+    (is (false? (triangle? 25 5 10)))
+    (is (false? (triangle? 25 10 5)))
+
+    (is (true? (triangle? 3 4 5)))
+    (is (true? (triangle? 3 5 4)))
+    (is (true? (triangle? 4 3 5)))
+    (is (true? (triangle? 4 5 3)))
+    (is (true? (triangle? 5 3 4)))
+    (is (true? (triangle? 5 4 3)))))
+
+(deftest reorder-triple-test
+  (testing "reorder triple"
+    (is (= [[1 4 7]
+            [2 5 8]
+            [3 6 9]]
+           (reorder-triple [[1 2 3]
+                            [4 5 6]
+                            [7 8 9]])))))
+
+(deftest transpose-test
+  (testing "transpose"
+    (is (= [[1 4 7]
+            [2 5 8]
+            [3 6 9]]
+           (transpose [[1 2 3]
+                       [4 5 6]
+                       [7 8 9]])))))
