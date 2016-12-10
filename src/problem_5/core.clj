0bf0f36436862dea705e17089dcc0f670f4d8436
Ryan Mulcahy <ryan@primeacademy.io>
2016-12-10 12:31:56 -0600

    Problem 5, part 2

--- src/problem_5/core.clj
+++ src/problem_5/core.clj
@@ -7,10 +7,24 @@
 (defn nth-hash [n]
   (md5 (join [input n])))
 
+(defn int-or-nil [char]
+  (try (Integer/valueOf (str char))
+       (catch Exception e nil)))
+
+(defn collect-password
+  [password [position value]]
+  (if  (< (count password) 8)
+    (update password position #(or % value))
+    (reduced password)))
+
 (->> (range)
      (map nth-hash)
      (filter #(starts-with? % "00000"))
-     (take 8)
-     (map #(nth % 5))
+     (filter #(when-let [position (int-or-nil (nth % 5))]
+                (and (>= position 0)
+                     (<= position 8))))
+     (map #(vector (int-or-nil (nth % 5)) (nth % 6)))
+     (reduce collect-password {})
+     sort
+     vals
      join)
-
