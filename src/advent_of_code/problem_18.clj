ceab21a32619002a67343d08c65cb6e3557f6d9e
Ryan Mulcahy <ryan@primeacademy.io>
2016-12-29 03:20:26 -0600

    Minor formatting changes

--- src/advent_of_code/problem_18.clj
+++ src/advent_of_code/problem_18.clj
@@ -5,40 +5,9 @@
 
 (def puzzle-input ".^^^^^.^^.^^^.^...^..^^.^.^..^^^^^^^^^^..^...^^.^..^^^^..^^^^...^.^.^^^^^^^^....^..^^^^^^.^^^.^^^.^^")
 
-(defn dot-or-carrot? [char]
-  (case (str char)
-    ("." "^") true
-    false))
-
-(s/def ::row
-  (s/with-gen (s/coll-of (s/and string?
-                                #(= 1 (count %))
-                                dot-or-carrot?))
-    #(gen/not-empty (gen/vector (s/gen #{"." "^"})))))
-
-;(s/exercise ::row)
-
-(s/def ::input
-  (s/with-gen (s/and string?
-                     not-empty
-                     #(every? dot-or-carrot? %))
-    #(gen/fmap
-     join
-     (gen/not-empty (gen/vector (s/gen #{"." "^"}))))))
-
-;(s/exercise ::input)
-
-(s/fdef input->row
-        :args (s/cat :input ::input)
-        :ret  ::row
-        :fn   #(= (count (:ret %))
-                  (count (-> % :args :input))))
 (defn input->row
   [input]
   (split input #""))
-
-;(s/exercise-fn `input->row)
-
 
 ;Then, a new tile is a trap only in one of the following situations:
 ;Its left and center tiles are traps, but its right tile is not.
@@ -56,24 +25,13 @@
 
     "."))
 
-;(tuple->tile ["." "^" "."])
-
-(s/fdef next-row
-        :args (s/cat :row ::row)
-        :ret  ::row)
 (defn next-row
   [row]
   (let [padded (conj (vec (cons  "." row)) ".")]
     (->> padded
          (partition 3 1)
-         (mapv tuple->tile)
-         )))
+         (mapv tuple->tile))))
 
-;(next-row ["." "." "^" "^" "."])
-
-(s/fdef next-input
-        :args (s/cat :input ::input)
-        :ret ::input)
 (defn next-input
   [input]
   (->> input
@@ -81,26 +39,60 @@
        next-row
        join))
 
-;(s/exercise-fn `next-row)
-
-(s/def ::input-and-number (s/cat :input ::input :n pos-int?))
-
-(s/fdef create-n-rows
-        :args ::input-and-number
-        :ret  (s/coll-of ::input)
-        :fn   #(= (-> % :args :n)
-                  (count (:ret %))))
 (defn create-n-rows [input n]
   (take n (iterate next-input input)))
 
-;(s/exercise-fn create-n-rows)
-
-(s/fdef number-of-safe-tiles
-        :args ::input-and-number
-        :ret  integer?)
 (defn number-of-safe-tiles
   [input n]
   (->> (create-n-rows input n)
        join
        (filter #(= \. %))
        count))
+
+
+;; specs
+
+
+(defn- dot-or-carrot? [char]
+  (case (str char)
+    ("." "^") true
+    false))
+
+(s/def ::row
+  (s/with-gen (s/coll-of (s/and string?
+                                #(= 1 (count %))
+                                dot-or-carrot?))
+    #(gen/not-empty (gen/vector (s/gen #{"." "^"})))))
+
+(s/def ::input
+  (s/with-gen (s/and string?
+                     not-empty
+                     #(every? dot-or-carrot? %))
+    #(gen/fmap
+     join
+     (gen/not-empty (gen/vector (s/gen #{"." "^"}))))))
+
+(s/fdef input->row
+        :args (s/cat :input ::input)
+        :ret  ::row
+        :fn   #(= (count (:ret %))
+                  (count (-> % :args :input))))
+
+(s/fdef next-row
+        :args (s/cat :row ::row)
+        :ret  ::row)
+
+(s/fdef next-input
+        :args (s/cat :input ::input)
+        :ret ::input)
+
+(s/def ::input-and-number (s/cat :input ::input :n pos-int?))
+(s/fdef create-n-rows
+        :args ::input-and-number
+        :ret  (s/coll-of ::input)
+        :fn   #(= (-> % :args :n)
+                  (count (:ret %))))
+
+(s/fdef number-of-safe-tiles
+        :args ::input-and-number
+        :ret  integer?)
